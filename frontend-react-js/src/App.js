6b6fcddb22b2527cb3b3320ccc1ecfacca6116fe
kodexkate <122316410+kodexkate@users.noreply.github.com>
2023-03-11 23:46:11 +0000

    update app.js file

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
 import NotificationsFeedPage from './pages/NotificationsFeedPage';
+import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
