1e7de9816de0a3d16b814a72b7407bcb8509b141
kodexkate <katelyn_nett06@yahoo.com>
2023-03-22 20:15:21 +0000

    add spans and attributes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,8 +7,10 @@ class HomeActivities:
   def run():
   
     with tracer.start_as_current_span("home-activities-mock-data"):
-     now = datetime.now(timezone.utc).astimezone()
-    results = [{
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+      results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
       'message': 'Cloud is fun!',
@@ -47,4 +49,6 @@ class HomeActivities:
       'replies': []
     }
     ]
+    span.set_attribute("app.result_length", len(results))
     return results
+  
