36dba1071e5634c40b18842ff2ba2eed22ac64bb
kodexkate <katelyn_nett06@yahoo.com>
2023-03-29 22:01:38 +0000

    remove aws xray contents

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,10 +33,10 @@ provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
-# X-RAY ---------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+# AWS X-RAY ---------
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+#XRayMiddleware(app, xray_recorder)
 
 # Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
