4216677cab9291238475257991fc14fc11c5f7ad
kodexkate <katelyn_nett06@yahoo.com>
2023-04-21 21:49:30 +0000

    updates to db

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Kate Nettles', 'katelyn_nett06@yahoo.com', 'Kate', 'MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'Kate' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
