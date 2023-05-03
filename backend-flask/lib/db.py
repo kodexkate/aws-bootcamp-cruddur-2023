9a8078eecb0ea7ef4ddc40d1f72cccd2c554d3f0
kodexkate <katelyn_nett06@yahoo.com>
2023-05-03 21:39:19 +0000

    Add initialization script to install boto3

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -35,13 +35,13 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
 
-  def print_sql(self,title,sql):
-    cyan = '\ 033[96m'
-    no_color = '\ 033[0m'
+  def print_sql(self,title,sql,params={}):
+    cyan = '\033[96m'
+    no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql,params)
   def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql)
+    self.print_sql('commit with returning',sql,params)
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
@@ -58,8 +58,8 @@ class Db:
     except Exception as err:
       self.print_sql_err(err)
   # when we want to return a json object
-  def query_array_json(self,sql,params={}):
-    self.print_sql('array',sql)
+    def query_array_json(self,sql,params={}):
+     self.print_sql('array',sql,params)
 
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -70,18 +70,26 @@ class Db:
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
 
-    self.print_sql('json',sql)
-    self.print_params(params)
-    wrapped_sql = self.query_wrap_object(sql)
+      self.print_sql('json',sql,params)
+      self.print_params(params)
+      wrapped_sql = self.query_wrap_object(sql)
 
-    with self.pool.connection() as conn:
-      with conn.cursor() as cur:
+      with self.pool.connection() as conn:
+       with conn.cursor() as cur:
         cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         if json == None:
           "{}"
         else:
           return json[0]
+  def query_value(self,sql,params={}):
+    self.print_sql('value',sql,params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
+
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -89,13 +97,15 @@ class Db:
     ) object_row);
     """
     return sql
-  def query_wrap_array(self,template):
-    sql = f"""
+
+    def query_wrap_array(self,template):
+     sql = f"""
     (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
     {template}
     ) array_row);
     """
     return sql
+
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
